388479246fa000f5b0f914a4e6b29442a7f9b825
Eddie Chen <livehighvu06@gmail.com>
2023-01-04 17:03:56 +0800

    寶可夢圖鑑 / 初版

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { useQuery, QueryClient, QueryClientProvider } from "react-query";
+const BASEURL = "https://pokeapi.co/api/v2/pokemon?limit=20";
+const queryClient = new QueryClient();
+const fetchData = async function (url) {
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(response.status);
+  }
+  const data = await response.json();
+  return data;
+};
 
-function App() {
+function Pokemon({ pokemon }) {
+  const { url } = pokemon;
+  const {
+    data: pokemonData,
+    isLoading,
+    isError,
+  } = useQuery(["pokemonInfo", url], () => fetchData(url));
+
+  const speciesURL = pokemonData?.species.url;
+
+  const {
+    data: pokemonSpecies,
+    isLoading: speciesIsLoading,
+    isError: speciesIsError,
+  } = useQuery(["pokemonDetail", speciesURL], () => fetchData(speciesURL));
+  if (isLoading || speciesIsLoading) return "Loading";
+  if (isError || speciesIsError) return "Error";
+
+  const { sprites } = pokemonData;
+  const img = sprites.front_default;
+  const { name } = pokemonSpecies.names.find(
+    (pokemon) => pokemon.language.name === "zh-Hant"
+  );
+  console.log(name);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <li>
+      <img src={img} alt={name} />
+      <span>{name}</span>
+    </li>
+  );
+}
+function Pokemons({ pokemons }) {
+  return (
+    <ol>
+      {pokemons?.map((pokemon) => {
+        return <Pokemon pokemon={pokemon} key={pokemon.url} />;
+      })}
+    </ol>
   );
 }
 
-export default App;
+export default function App() {
+  const [pokemons, setPokemons] = useState(null);
+
+  useEffect(() => {
+    fetchData(BASEURL).then(({ results }) => setPokemons(results));
+  }, []);
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <Pokemons pokemons={pokemons} />
+      </div>
+    </QueryClientProvider>
+  );
+}
