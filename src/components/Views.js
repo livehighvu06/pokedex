823ebd82e1fad0a28d6b77d01a124f2ec43ff0a8
Eddie Chen <livehighvu06@gmail.com>
2023-01-09 17:41:56 +0800

    修正滾動載入後URL沒有刷新的問題

--- src/components/Views.js
+++ src/components/Views.js
@@ -2,29 +2,17 @@ import { useInfiniteQuery } from "react-query";
 import InfiniteScroll from "react-infinite-scroller";
 import PokemonList from "./PokemonList";
 import fetchData from "../API";
-let LIMIT = 20;
-// let pageParam = 20;
-
-// const fetchUrl = async (pageParam) => {
-//   const endpoint = new URL("https://pokeapi.co/api/v2/pokemon");
-//   endpoint.search = new URLSearchParams({
-//     limit: pageParam,
-//   });
-//   console.log(endpoint);
-//   const response = await fetch(endpoint);
-//   return response.json();
-// };
-async function fetchPokemons({
-  pageParam = `https://pokeapi.co/api/v2/pokemon?limit=${LIMIT}`,
-}) {
-  return fetchData(pageParam);
-}
+const LIMIT = 20;
 export default function Pokemons() {
   const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } =
-    useInfiniteQuery(["pokemonDataPage"], fetchPokemons, {
-      getNextPageParam: (lastPage) => lastPage.next,
-      // getPreviousPageParam: (firstPage) => firstPage.previous,
-    });
+    useInfiniteQuery(
+      "pokemonDataPage",
+      ({ pageParam = `https://pokeapi.co/api/v2/pokemon?limit=${LIMIT}` }) =>
+        fetchData(pageParam),
+      {
+        getNextPageParam: (lastPage) => lastPage.next,
+      }
+    );
   if (isLoading) return "初始化";
   return (
     <>
