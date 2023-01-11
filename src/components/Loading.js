a243d6d42369dd32b25cae53b7345af19928b838
Eddie Chen <livehighvu06@gmail.com>
2023-01-11 22:33:18 +0800

    新增loading gif

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import loadingImg from "../images/loading.gif";
+
+function Loading() {
+  return (
+    <div>
+      <img src={loadingImg} alt="" />
+    </div>
+  );
+}
+
+export default Loading;
