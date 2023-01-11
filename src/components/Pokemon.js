09c1ff30a912c167ac05a7dcd268ef93c87232a5
Eddie Chen <livehighvu06@gmail.com>
2023-01-11 23:22:19 +0800

    新增Pokemon背景色跟排版

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,5 +1,29 @@
 import { useQuery } from "react-query";
 import fetchData from "../API.js";
+function getColor(type) {
+  const colors = new Map([
+    ["bug", "#a6b91a"],
+    ["dark", "#705746"],
+    ["dragon", "#6f35fc"],
+    ["electric", "#f7d02c"],
+    ["fairy", "#d685ad"],
+    ["fighting", "#c22e28"],
+    ["fire", "#ee8130"],
+    ["flying", "#a98ff3"],
+    ["ghost", "#735797"],
+    ["grass", "#7ac74c"],
+    ["ground", "#e2bf65"],
+    ["ice", "#96d9d6"],
+    ["normal", "#a8a77a"],
+    ["poison", "#a33ea1"],
+    ["psychic", "#f95587"],
+    ["rock", "#b6a136"],
+    ["steel", "#b7b7ce"],
+    ["water", "#6390f0"],
+  ]);
+
+  return colors.get(type) || "#777";
+}
 export default function Pokemon({ pokemon }) {
   const { url } = pokemon;
   const {
@@ -18,16 +42,28 @@ export default function Pokemon({ pokemon }) {
   if (isLoading || speciesIsLoading) return "Loading";
   if (isError || speciesIsError) return "Error";
 
-  const { sprites } = pokemonData;
-  const img = sprites.front_default;
+  // const { sprites } = pokemonData;
+  // const img = sprites.front_default;
+  const { id, types } = pokemonData;
+  console.log(pokemonData);
+  const img = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
+  const color = getColor(types[0].type.name);
+  const gradient = types.map((type) => getColor(type.type.name)).join(", ");
   const { name } = pokemonSpecies.names.find(
     (pokemon) => pokemon.language.name === "zh-Hant"
   );
+  console.log(color, gradient);
 
   return (
-    <li>
-      <img src={img} alt={name} />
-      <span>{name}</span>
+    <li
+      className="p-6 border-2 rounded-lg"
+      style={{
+        backgroundColor: color,
+        backgroundImage: `linear-gradient(45deg, ${gradient})`,
+      }}
+    >
+      <img className="mx-auto w-60" src={img} alt={name} />
+      {/* <span className="text-white font-bold text-xl block mt-6">{name}</span> */}
     </li>
   );
 }
--- src/components/Views.js
+++ src/components/Views.js
@@ -2,30 +2,32 @@ import { useInfiniteQuery } from "react-query";
 import InfiniteScroll from "react-infinite-scroller";
 import PokemonList from "./PokemonList";
 import fetchData from "../API";
+import Loading from "./Loading";
 const LIMIT = 20;
 export default function Pokemons() {
-  const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } =
-    useInfiniteQuery(
-      "pokemonDataPage",
-      ({ pageParam = `https://pokeapi.co/api/v2/pokemon?limit=${LIMIT}` }) =>
-        fetchData(pageParam),
-      {
-        getNextPageParam: (lastPage) => lastPage.next,
-      }
-    );
-  if (isLoading) return "初始化";
+  const { data, isLoading, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    "pokemonDataPage",
+    ({ pageParam = `https://pokeapi.co/api/v2/pokemon?limit=${LIMIT}` }) =>
+      fetchData(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next,
+    }
+  );
   return (
     <>
-      {isFetchingNextPage && "載入中~"}
-      <InfiniteScroll hasMore={hasNextPage} loadMore={fetchNextPage}>
-        <div>
-          <ol className="pokemon__list">
-            {data?.pages.map((page, index) => {
-              return <PokemonList pokemons={page.results} key={index} />;
-            })}
-          </ol>
-        </div>
-      </InfiniteScroll>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <InfiniteScroll hasMore={hasNextPage} loadMore={fetchNextPage}>
+          <div className="container mx-auto p-6">
+            <ol className="pokemon__list grid grid-cols-pokemon text-center gap-6 mb-6">
+              {data?.pages.map((page, index) => {
+                return <PokemonList pokemons={page.results} key={index} />;
+              })}
+            </ol>
+          </div>
+        </InfiniteScroll>
+      )}
     </>
   );
 }
